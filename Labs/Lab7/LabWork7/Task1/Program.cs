b919ce5b6f9b6b3ef6a8a1f59c6c38cdd3e20145
0109-06 <ispp4106>
2026-09-14 10:06:48 +0300

    Lab7 (1-2)

---
+++ Labs/Lab7/LabWork7/Task1/Program.cs
@@ -0,0 +1,48 @@
+﻿try
+{
+    Console.Write("Введите первое число: ");
+    double firstNum = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Введите второе число: ");
+    double secondNum = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine($"Сложение: {firstNum + secondNum}\n" +
+        $"Вычитание: {firstNum - secondNum}\n" +
+        $"Умножение: {firstNum * secondNum}\n" +
+        $"Деление: {firstNum / secondNum}\n");
+}
+catch(DivideByZeroException ex)
+{
+    Console.WriteLine($"{ex.Message} Ошибка");
+    LogErrors(ex);
+}
+catch(FormatException ex)
+{
+    Console.WriteLine($"{ex.Message} Ошибка");
+    LogErrors(ex);
+}
+catch (OverflowException ex)
+{
+    Console.WriteLine($"{ex.Message} Ошибка");
+    LogErrors(ex);
+}
+catch(NullReferenceException ex)
+{
+    Console.WriteLine($"{ex.Message} Ошибка");
+    LogErrors(ex);
+}
+catch(InvalidCastException ex)
+{
+    Console.WriteLine($"{ex.Message} Ошибка");
+    LogErrors(ex);
+}
+catch(Exception ex)
+{
+    Console.WriteLine($"{ex.Message} Ошибка");
+    LogErrors(ex);
+}
+
+static void LogErrors(Exception ex)
+{
+    string textFilePath = "log.txt";
+    File.AppendAllText(textFilePath, $"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] {ex.ToString()}\n\n");
+}
---
+++ Labs/Lab7/LabWork7/Task2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+string dirPath = @"C:\Program Files";
+DirectoryInfo currentDir = new DirectoryInfo(dirPath);
+int countFiles = 0;
+
+string textFilePath = "Dostoevsky.txt";
+int countWords = 0;
+
+string logFilePath = @"timings.log";
+
+Stopwatch stopwatch = new Stopwatch();
+
+
+stopwatch.Start();
+GetNewDirectory(currentDir);
+Console.WriteLine($"Всего файлов в папке {dirPath}: {countFiles}");
+stopwatch.Stop();
+LogOperations(logFilePath, nameof(GetNewDirectory));
+
+stopwatch.Start();
+ReadWordsCount(textFilePath, countWords);
+stopwatch.Stop();
+
+LogOperations(logFilePath, nameof(ReadWordsCount));
+
+void LogOperations(string logFilePath, string methodName) =>
+    File.AppendAllText(logFilePath, $"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] Operation={methodName}," +
+    $"Elapsed={stopwatch.ElapsedMilliseconds}\n\n");
+
+
+static void ReadWordsCount(string filePath, int countWords)
+{
+    if (!File.Exists(filePath))
+    {
+        Console.WriteLine("Файл не найден.");
+        return;
+    }
+
+    using (StreamReader reader = new StreamReader(filePath))
+    {
+        string line;
+
+        while ((line = reader.ReadLine()) != null)
+        {
+            string[] words = Regex.Split(line, @"\W+");
+
+            foreach (string word in words)
+                if (Regex.IsMatch(word, @"^[а-яА-Я]+$"))
+                    countWords++;
+        }
+    }
+    Console.WriteLine($"Количество слов в файле: {countWords}");
+}
+
+
+void GetNewDirectory(DirectoryInfo currentDir)
+{
+    try
+    {
+        FileInfo[] files = currentDir.GetFiles();
+
+        foreach (var file in files)
+            countFiles++;
+
+        DirectoryInfo[] subDirs = currentDir.GetDirectories();
+
+        foreach (var subDir in subDirs)
+            GetNewDirectory(subDir);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
